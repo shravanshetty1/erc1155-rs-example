70fa063e54889a16deb0dda3cae472f8b756876d
Shravan Shetty <shravanshetty322@gmail.com>
2022-06-14 01:56:17 +0530

    implementing contract

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,14 +1,33 @@
-use clap::{ArgMatches, Command};
+use borsh::{BorshDeserialize, BorshSerialize};
+use clap::Command;
+use sled::IVec;
+
+use solana_sdk::pubkey::Pubkey;
+use solana_sdk::signature::{Keypair, Signer};
+
 use std::error::Error;
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
+const IS_INITIALISED: &str = "is-initialised";
+const CLI_STATE: &str = "state";
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct State {
+    accounts: Vec<Pubkey>,
+    current: Pubkey,
+}
+
 fn main() -> Result<()> {
+    let store = sled::open("~/.erc1155-rs/store")?;
+    if (store.get(IS_INITIALISED)?).is_none() {
+        initialize_store(store)?;
+    }
+
     let matches = Command::new("erc115-rs")
         .subcommand(
             Command::new("account")
                 .subcommand(Command::new("list"))
-                .subcommand(Command::new("add"))
                 .subcommand(Command::new("current"))
                 .subcommand(Command::new("set")),
         )
@@ -19,7 +38,7 @@ fn main() -> Result<()> {
 
     match matches.subcommand() {
         Some(("account", matches)) => match matches.subcommand() {
-            Some(("list", matches)) => {
+            Some(("list", _matches)) => {
                 println!("hello")
             }
             _ => {}
@@ -27,5 +46,19 @@ fn main() -> Result<()> {
         _ => {}
     }
 
+    Ok(())
+}
+
+pub fn initialize_store(store: sled::Db) -> crate::Result<()> {
+    let alice = Keypair::new();
+    let bob = Keypair::new();
+
+    let cli_state: State = State {
+        accounts: vec![alice.pubkey(), bob.pubkey()],
+        current: alice.pubkey(),
+    };
+    store.insert(CLI_STATE, IVec::from(borsh::to_vec(&cli_state)?))?;
+
+    store.insert(IS_INITIALISED, IVec::from("true"))?;
     Ok(())
 }
--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,18 +1,126 @@
+use borsh::BorshDeserialize;
+use sled::IVec;
+
 use solana_sdk::pubkey::Pubkey;
+use std::collections::{HashMap, HashSet};
 use std::error::Error;
 
 // erc1155 interface from https://eips.ethereum.org/EIPS/eip-1155
 pub trait ERC1155 {
     fn safe_batch_transfer_from(
+        &self,
         from: Pubkey,
         to: Pubkey,
         ids: Vec<u128>,
         values: Vec<u128>,
         data: Vec<u8>,
     ) -> crate::Result<()>;
-    fn balance_of_batch(owners: Vec<Pubkey>, ids: Vec<u128>) -> crate::Result<u128>;
-    fn set_approval_for_all(operator: Pubkey, approved: bool) -> crate::Result<()>;
-    fn is_approved_for_all(owner: Pubkey, operator: Pubkey) -> crate::Result<bool>;
+    fn balance_of_batch(&self, owners: Vec<Pubkey>, ids: Vec<u128>) -> crate::Result<Vec<u128>>;
+    fn set_approval_for_all(&self, operator: Pubkey, approved: bool) -> crate::Result<()>;
+    fn is_approved_for_all(&self, owner: Pubkey, operator: Pubkey) -> crate::Result<bool>;
 }
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
+
+pub struct ERC1155Implementation {
+    store: sled::Db,
+    caller: Pubkey,
+}
+
+impl ERC1155Implementation {
+    pub fn new(store: sled::Db, caller: Pubkey) -> Self {
+        ERC1155Implementation { store, caller }
+    }
+
+    pub fn create_token(&self, supply: u128) {}
+}
+
+const ACCOUNT_PREFIX: &str = "account";
+
+#[derive(borsh::BorshSerialize, borsh::BorshDeserialize, Default)]
+pub struct Account {
+    approvals: HashSet<Pubkey>,
+    balances: HashMap<u128, u128>,
+}
+
+impl Account {
+    fn get_from_db(store: &sled::Db, address: Pubkey) -> crate::Result<Account> {
+        Ok(Account::try_from_slice(
+            store
+                .get(format!("{}-{}", ACCOUNT_PREFIX, address))?
+                .unwrap_or_default()
+                .as_ref(),
+        )
+        .unwrap_or_default())
+    }
+
+    fn set_to_db(store: &sled::Db, address: Pubkey, state: Account) -> crate::Result<()> {
+        store.insert(
+            format!("{}-{}", ACCOUNT_PREFIX, address),
+            IVec::from(borsh::to_vec(&state)?),
+        )?;
+        Ok(())
+    }
+}
+
+impl ERC1155 for ERC1155Implementation {
+    // TODO check if caller can execute transaction
+    fn safe_batch_transfer_from(
+        &self,
+        from: Pubkey,
+        to: Pubkey,
+        ids: Vec<u128>,
+        values: Vec<u128>,
+        _data: Vec<u8>,
+    ) -> Result<()> {
+        let mut from_state: Account = Account::get_from_db(&self.store, from.clone())?;
+        let mut to_state: Account = Account::get_from_db(&self.store, to.clone())?;
+
+        for i in 0..ids.len() {
+            let id = ids.get(i).ok_or("index out of bounds")?;
+            let val = values.get(i).ok_or("index out of bounds")?;
+
+            // TODO not sure if this works
+            if from_state.balances.get(id).unwrap_or(&0) > val {
+                *from_state.balances.get_mut(id).unwrap_or(&mut 0) -= val;
+                *to_state.balances.get_mut(id).unwrap_or(&mut 0) += val;
+            } else {
+                return Err(format!("insufficient balance for token num {}", id).into());
+            }
+        }
+
+        Account::set_to_db(&self.store, from, from_state)?;
+        Account::set_to_db(&self.store, to, to_state)?;
+
+        Ok(())
+    }
+
+    fn balance_of_batch(&self, owners: Vec<Pubkey>, ids: Vec<u128>) -> Result<Vec<u128>> {
+        let mut balances: Vec<u128> = Vec::new();
+        for i in 0..owners.len() {
+            let owner = owners.get(i).ok_or("index out of bounds")?;
+            let id = ids.get(i).ok_or("index out of bounds")?;
+
+            let mut owner_state = Account::get_from_db(&self.store, owner.clone())?;
+            balances.push(owner_state.balances.get(id).unwrap_or(&0).clone());
+        }
+
+        Ok(balances)
+    }
+
+    fn set_approval_for_all(&self, operator: Pubkey, approved: bool) -> Result<()> {
+        let mut caller_state = Account::get_from_db(&self.store, self.caller.clone())?;
+        if approved {
+            caller_state.approvals.insert(operator);
+        } else {
+            caller_state.approvals.remove(&operator);
+        }
+        Account::set_to_db(&self.store, self.caller.clone(), caller_state);
+        Ok(())
+    }
+
+    fn is_approved_for_all(&self, owner: Pubkey, operator: Pubkey) -> Result<bool> {
+        let mut owner_state = Account::get_from_db(&self.store, owner.clone())?;
+        Ok(owner_state.approvals.get(&operator).is_some())
+    }
+}
