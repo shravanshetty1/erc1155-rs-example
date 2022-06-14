feba3ea0f7e3e762277dd9db65ecd44374e3ccd0
Shravan Shetty <shravanshetty322@gmail.com>
2022-06-14 08:04:09 +0530

    minor refactor

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,10 @@
-use borsh::{BorshDeserialize, BorshSerialize};
-use clap::{Arg, ArgMatches, Command};
+mod account;
+mod approval;
+mod balances;
+mod state;
+mod transfer;
+
+use clap::Command;
 use sled::IVec;
 use std::collections::HashSet;
 
@@ -8,20 +13,11 @@ use solana_sdk::signature::{Keypair, Signer};
 
 use std::error::Error;
 
-use contract::{ERC1155Implementation, ERC1155};
-use std::str::FromStr;
+use crate::state::State;
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 const IS_INITIALISED: &str = "is-initialised";
-const CLI_STATE: &str = "state";
-
-#[derive(BorshSerialize, BorshDeserialize, Debug)]
-pub struct State {
-    accounts: HashSet<Pubkey>,
-    current: Pubkey,
-}
-
 fn main() -> Result<()> {
     let store = sled::open("~/.erc1155-rs/store")?;
     if (store.get(IS_INITIALISED)?).is_none() {
@@ -29,132 +25,19 @@ fn main() -> Result<()> {
     }
 
     let matches = Command::new("erc115-rs")
-        .subcommand(
-            Command::new("account")
-                .subcommand(Command::new("list"))
-                .subcommand(Command::new("current"))
-                .subcommand(Command::new("set").arg(Arg::new("account").required(true)))
-                .subcommand(Command::new("state").arg(Arg::new("account").required(true))),
-        )
-        .subcommand(Command::new("transfer").args(vec![
-            Arg::new("from").long("from").takes_value(true).required(true),
-            Arg::new("to").long("to").takes_value(true).required(true),
-            Arg::new("token-id").long("token-id").takes_value(true).required(true),
-            Arg::new("amount").long("amount").takes_value(true).required(true),
-        ]))
-        .subcommand(Command::new("balances").args(vec![
-            Arg::new("owner").long("owner").takes_value(true).required(true),
-            Arg::new("token-id").long("token-id").takes_value(true).required(true),
-        ]))
-        .subcommand(
-            Command::new("approval")
-                .subcommand(
-                    Command::new("add").arg(Arg::new("address").takes_value(true).required(true)),
-                )
-                .subcommand(
-                    Command::new("remove")
-                        .arg(Arg::new("address").takes_value(true).required(true)),
-                ),
-        )
+        .subcommand(account::command())
+        .subcommand(transfer::command())
+        .subcommand(balances::command())
+        .subcommand(approval::command())
         .get_matches();
 
-    let mut state: State =
-        State::try_from_slice(store.get(CLI_STATE)?.unwrap_or_default().as_ref())?;
-
-    match matches.subcommand() {
-        Some(("account", matches)) => match matches.subcommand() {
-            Some(("list", _matches)) => list_accounts(store)?,
-            Some(("current", _matches)) => current_account(store)?,
-            Some(("state", matches)) => {
-                let contract = ERC1155Implementation::new(store, state.current.clone());
-                let account =
-                    Pubkey::from_str(matches.value_of("account").ok_or("account is mandatory")?)?;
-                let account_state = contract.account_state(account)?;
-                println!("{:#?}", account_state)
-            }
-            Some(("set", matches)) => {
-                let account = matches
-                    .value_of("account")
-                    .ok_or("account arg is required")?;
-                let account = Pubkey::from_str(account)?;
-                set_account(store, account)?
-            }
-            _ => {}
-        },
-        Some(("transfer", matches)) => {
-            let from = Pubkey::from_str(
-                matches
-                    .value_of("from")
-                    .ok_or("from account is mandatory")?,
-            )?;
-            let to = Pubkey::from_str(matches.value_of("to").ok_or("to account is mandatory")?)?;
-            let token_id = matches
-                .value_of("token-id")
-                .ok_or("token-id is mandatory")?
-                .parse::<u128>()?;
-            let amount = matches
-                .value_of("amount")
-                .ok_or("amount is mandatory")?
-                .parse::<u128>()?;
-
-            let contract = ERC1155Implementation::new(store, state.current.clone());
-            contract.safe_batch_transfer_from(from, to, vec![token_id], vec![amount], vec![])?;
-        }
-        Some(("balances", matches)) => {
-            let contract = ERC1155Implementation::new(store, state.current.clone());
-            let owner = Pubkey::from_str(matches.value_of("owner").ok_or("owner is mandatory")?)?;
-            let token_id = matches
-                .value_of("token-id")
-                .ok_or("token-id is mandatory")?
-                .parse::<u128>()?;
-            let balance = contract.balance_of_batch(vec![owner], vec![token_id])?;
-            println!("{}", balance.first().unwrap_or(&0));
-        }
-        Some(("approval", matches)) => match matches.subcommand() {
-            Some(("add", matches)) => {
-                let address =
-                    Pubkey::from_str(matches.value_of("address").ok_or("address is mandatory")?)?;
-
-                let contract = ERC1155Implementation::new(store, state.current.clone());
-                contract.set_approval_for_all(address, true)?;
-            }
-            Some(("remove", matches)) => {
-                let address =
-                    Pubkey::from_str(matches.value_of("address").ok_or("address is mandatory")?)?;
-
-                let contract = ERC1155Implementation::new(store, state.current.clone());
-                contract.set_approval_for_all(address, false)?;
-            }
-            _ => {}
-        },
-        _ => {}
-    }
-
-    Ok(())
-}
-
-pub fn set_account(store: sled::Db, account: Pubkey) -> crate::Result<()> {
-    let mut state: State =
-        State::try_from_slice(store.get(CLI_STATE)?.unwrap_or_default().as_ref())?;
-    state.current = account;
-    state.accounts.insert(account);
-    store.insert(CLI_STATE, IVec::from(borsh::to_vec(&state)?))?;
-
-    Ok(())
-}
-
-pub fn current_account(store: sled::Db) -> crate::Result<()> {
-    let state: State = State::try_from_slice(store.get(CLI_STATE)?.unwrap_or_default().as_ref())?;
-    println!("{:#?}", state.current);
-
-    Ok(())
-}
-
-pub fn list_accounts(store: sled::Db) -> crate::Result<()> {
-    let state: State = State::try_from_slice(store.get(CLI_STATE)?.unwrap_or_default().as_ref())?;
-    println!("{:#?}", state.accounts);
-
-    Ok(())
+    return match matches.subcommand() {
+        Some(("account", matches)) => account::handle(store, matches),
+        Some(("transfer", matches)) => transfer::handle(store, matches),
+        Some(("balances", matches)) => balances::handle(store, matches),
+        Some(("approval", matches)) => approval::handle(store, matches),
+        _ => Ok(()),
+    };
 }
 
 pub fn initialize_store(store: &sled::Db) -> crate::Result<()> {
@@ -168,7 +51,7 @@ pub fn initialize_store(store: &sled::Db) -> crate::Result<()> {
         accounts,
         current: alice.pubkey(),
     };
-    store.insert(CLI_STATE, IVec::from(borsh::to_vec(&cli_state)?))?;
+    State::set_to_db(store, cli_state)?;
 
     let contract = contract::ERC1155Implementation::new(store.clone(), alice.pubkey());
     contract.create_token(100000)?;
--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,8 +1,8 @@
-use borsh::BorshDeserialize;
-use sled::IVec;
+mod state;
 
+use crate::state::{Account, Contract};
 use solana_sdk::pubkey::Pubkey;
-use std::collections::{HashMap, HashSet};
+
 use std::error::Error;
 
 // erc1155 interface from https://eips.ethereum.org/EIPS/eip-1155
@@ -48,54 +48,6 @@ impl ERC1155Implementation {
 
     pub fn account_state(&self, address: Pubkey) -> crate::Result<Account> {
         Account::get_from_db(&self.store, address)
-    }
-}
-
-const CONTRACT_PREFIX: &str = "contract";
-#[derive(borsh::BorshSerialize, borsh::BorshDeserialize, Default)]
-pub struct Contract {
-    token_count: u128,
-}
-
-impl Contract {
-    fn get_from_db(store: &sled::Db) -> crate::Result<Contract> {
-        Ok(
-            Contract::try_from_slice(store.get(CONTRACT_PREFIX)?.unwrap_or_default().as_ref())
-                .unwrap_or_default(),
-        )
-    }
-
-    fn set_to_db(store: &sled::Db, state: Contract) -> crate::Result<()> {
-        store.insert(CONTRACT_PREFIX, IVec::from(borsh::to_vec(&state)?))?;
-        Ok(())
-    }
-}
-
-const ACCOUNT_PREFIX: &str = "account";
-
-#[derive(borsh::BorshSerialize, borsh::BorshDeserialize, Default, Debug)]
-pub struct Account {
-    approvals: HashSet<Pubkey>,
-    balances: HashMap<u128, u128>,
-}
-
-impl Account {
-    fn get_from_db(store: &sled::Db, address: Pubkey) -> crate::Result<Account> {
-        Ok(Account::try_from_slice(
-            store
-                .get(format!("{}-{}", ACCOUNT_PREFIX, address))?
-                .unwrap_or_default()
-                .as_ref(),
-        )
-        .unwrap_or_default())
-    }
-
-    fn set_to_db(store: &sled::Db, address: Pubkey, state: Account) -> crate::Result<()> {
-        store.insert(
-            format!("{}-{}", ACCOUNT_PREFIX, address),
-            IVec::from(borsh::to_vec(&state)?),
-        )?;
-        Ok(())
     }
 }
 
